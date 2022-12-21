5e7aabd023d537443aa1946b83d06fc09945d46d
qqqqqqqqqqqqqqqqqqqq <qqqqqqqqqqqqqqqqqqqq@DESKTOP-Q88ETHJ>
2022-12-21 14:37:06 +0530

    commiting eclipse_config

---
+++ src/test/java/com/ecomm/eclipse/IdeEclipse.java
@@ -0,0 +1,13 @@
+package com.ecomm.eclipse;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class IdeEclipse {
+	@Test(groups="sanity")
+	public void testngide()
+	{
+		Reporter.log("hi everyone",true);
+	}
+
+}
